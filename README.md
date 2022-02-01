646237502e1d362c711bd6a998cb40eb492f905e
Denis <omelched@gmail.com>
2022-02-01 13:48:20 +0300

    various changes: change url to project-wise (multiapp compatibility) move get_access_token to __init__ scope (optimization) data in _build_notification_payload new method notify_multiple_devices

--- README.md
+++ README.md
@@ -1,29 +1,32 @@
 # Python Huawei Cloud Messaging
 
-Wrapper of huawei cloud messaging [(Push Kit)](https://developer.huawei.com/consumer/en/hms/huawei-pushkit/) for sending push notification using python.
+my fork of Wrapper of huawei cloud messaging [(Push Kit)](https://developer.huawei.com/consumer/en/hms/huawei-pushkit/) for sending push notification using python.
 
 ## Home page
 
-https://pypi.org/project/pyhcm/
+https://pypi.org/project/pyhcm/ — original project
 
 ## Install
-```pip install pyhcm```
+```pip install git+https://github.com/omelched/python-huawei-cloud-messaging```
 
 ## Example
 
-### Send notification to topic
-```
-CLIENT_ID = "00000000"
-CLIENT_SECRET = "0000000000000000000000000000000000000000000000000000000000000000"
-
-HCMNotification(CLIENT_ID, CLIENT_SECRET).notify_topic_subscribers('test_topic', 'test title', 'test body')
-```
-
-### Send notification to user
+### Send notification to device
 
 ```
 CLIENT_ID = "00000000"
 CLIENT_SECRET = "0000000000000000000000000000000000000000000000000000000000000000"
+PROJECT_ID = "000000000000000000"
 
-HCMNotification(CLIENT_ID, CLIENT_SECRET).notify_single_device('token_xyz', 'test title', 'test body')
+HCMNotification(CLIENT_ID, CLIENT_SECRET, PROJECT_ID).notify_single_device('token_xyz', 'test title', 'test body')
+```
+
+### Send notification to multiple devices
+
+```
+CLIENT_ID = "00000000"
+CLIENT_SECRET = "0000000000000000000000000000000000000000000000000000000000000000"
+PROJECT_ID = "000000000000000000"
+
+HCMNotification(CLIENT_ID, CLIENT_SECRET, PROJECT_ID).notify_multiple_devices(['token_1', 'token_2'], 'test title', 'test body')
 ```
--- pyhcm/pyhcm.py
+++ pyhcm/pyhcm.py
@@ -6,37 +6,50 @@ import json
 
 class HCMNotification:
 
-    def __init__(self, client_id: str, client_secret: str):
-        self.client_id = client_id
-        self.client_secret = client_secret
+    def __init__(self, client_id: str, client_secret: str, project_id: str):
         self.url_auth = 'https://oauth-login.cloud.huawei.com/oauth2/v2/token'
-        self.url_push = f'https://push-api.cloud.huawei.com/v1/{client_id}/messages:send'
+        # self.url_push = f'https://push-api.cloud.huawei.com/v1/{client_id}/messages:send'
+        self.url_push = f'https://push-api.cloud.huawei.com/v2/{project_id}/messages:send'
+        self.access_token = self.get_access_token(client_id, client_secret)
 
-    def get_access_token(self):
+    def get_access_token(self, client_id, client_secret):
         request = Request(
             self.url_auth,
-            data=urlencode({"grant_type": "client_credentials", "client_id": self.client_id,
-                            "client_secret": self.client_secret}).encode(),
+            data=urlencode(
+                {
+                    "grant_type": "client_credentials",
+                    "client_id": client_id,
+                    "client_secret": client_secret
+                }
+            ).encode(),
             headers={"Content-type": "application/x-www-form-urlencoded"}
         )
         return json.loads(urlopen(request).read())['access_token']
 
-    def _build_notification_payload(
-            self, topic: str, token: str, title: str, message_body: str):
+    @staticmethod
+    def _build_notification_payload(registration_ids=None, topic=None, message_title=None, message_body=None,
+                                    data: dict = None):
+        payload = dict()
+
+        if registration_ids:
+            payload['token'] = registration_ids
+        else:
+            payload['topic'] = topic
+
+        payload['data'] = json.dumps(data) if data else None
+
+        payload['android'] = {}
+        payload['android']['notification'] = {}
+        if message_title:
+            payload['android']['notification']['title'] = message_title
+        if message_body:
+            payload['android']['notification']['body'] = message_body
+        payload['android']['notification']['click_action'] = {
+            'type': 3,
+        }
+
         return {
-            'message': {
-                'topic': topic,
-                'token': [token] if token is not None else None,
-                'android': {
-                    'notification': {
-                        'title': title,
-                        'body': message_body,
-                        'click_action': {
-                            'type': 3
-                        }
-                    }
-                }
-            }
+            'message': payload,
         }
 
     def _send_notification_request(self, payload: object):
@@ -44,18 +57,36 @@ class HCMNotification:
             self.url_push,
             data=json.dumps(payload).encode(),
             headers={"Content-type": "application/json",
-                     "Authorization": f'Bearer {self.get_access_token()}'}
+                     "Authorization": f'Bearer {self.access_token}'}
         )
         response = json.loads(urlopen(request).read())
-        return response['msg'].lower() == 'success'
+        return response
 
     def notify_topic_subscribers(
-            self, topic: str, title: str, message_body: str):
+            self, topic: str, message_title: str, message_body: str):
         payload = self._build_notification_payload(
-            topic, None, title, message_body)
+            topic=topic,
+            message_title=message_title,
+            message_body=message_body
+        )
         return self._send_notification_request(payload)
 
-    def notify_single_device(self, token: str, title: str, message_body: str):
+    def notify_single_device(self, registration_id: str, message_title: str = None, message_body: str = None,
+                             data: dict = None):
         payload = self._build_notification_payload(
-            None, token, title, message_body)
+            registration_ids=[registration_id],
+            message_title=message_title,
+            message_body=message_body,
+            data=data
+        )
+        return self._send_notification_request(payload)
+
+    def notify_multiple_devices(self, registration_ids: list[str], message_title: str = None, message_body: str = None,
+                               data: dict = None):
+        payload = self._build_notification_payload(
+            registration_ids=registration_ids,
+            message_title=message_title,
+            message_body=message_body,
+            data=data
+        )
         return self._send_notification_request(payload)
